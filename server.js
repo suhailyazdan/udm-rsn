2d1465a8f42cbbc56d80300ab82429ed65797bd1
Administrator <Administrator@BLRTRAINING1.COM>
2023-04-18 09:48:43 +0530

    Added the random string function to the server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+module.exports = {
+    RandomStr: function(num) {
+        var string = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var str = '';
+
+        var i = 0;
+        while(i < num){
+            str += string.charAt(Math.floor(Math.random() * string.length)); 
+            i++;
+        }
+
+        return str;
+    }
+}
